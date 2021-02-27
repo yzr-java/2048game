6bf21aa88f6b6ad4cbd79b18d0cf25728ad38e09
yuzhuoran <1150899369@qq.com>
2021-02-27 23:58:34 +0800

    1

---
+++ storage.js
@@ -0,0 +1,33 @@
+// 历史最高分
+const BestScoreKey = '2048BestScore';
+// 方格状态和分数
+const CellStateKey = '2048CellState';
+
+function Storage() {}
+
+// 存储方格状态和分数
+Storage.prototype.setCellState = function({ score, grid }) {
+    window.localStorage.setItem(
+        CellStateKey,
+        JSON.stringify({
+            score,
+            grid: grid.serialize()
+        })
+    );
+};
+
+// 获取方格信息
+Storage.prototype.getCellState = function() {
+    const cellState = window.localStorage.getItem(CellStateKey);
+    return cellState ? JSON.parse(cellState) : null;
+};
+
+//存储历史最高分
+Storage.prototype.setBestScore = function(bestScore) {
+    window.localStorage.setItem(BestScoreKey,bestScore);
+};
+
+//获取历史最高分
+Storage.prototype.getBestScore = function() {
+    return window.localStorage.getItem(BestScoreKey);
+}
